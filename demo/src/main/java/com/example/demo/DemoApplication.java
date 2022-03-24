60529a9c1f9f5817f1cace96208ed9dee1138258
ivan.monahhov@gmail.com <Rkka 1945>
2022-03-24 18:01:56 +0200

    now scheduled

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,19 +3,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import javax.xml.bind.JAXBException;
 
 
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) throws JAXBException {
 		SpringApplication.run(DemoApplication.class, args);
 
 		Marshal.jaxbObjectToXML();
-
-		Unmarshal.jaxbXmlFileToObject("customer.xml");
 	}
 
 //	@Bean
--- demo/src/main/java/com/example/demo/Unmarshal.java
+++ demo/src/main/java/com/example/demo/Unmarshal.java
@@ -1,34 +1,41 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
+@Component
 public class Unmarshal {
+    @Autowired
+    CustomerRepository customerRepository;
 
-public static void jaxbXmlFileToObject(String fileName) {
-    File xmlFile = new File(fileName);
+    @Scheduled(fixedRate = Long.MAX_VALUE)
+    public void jaxbXmlFileToObject() {
+        File xmlFile = new File("customer.xml");
 
-    JAXBContext jaxbContext;
-    try
-    {
-        //Create JAXB context
-        jaxbContext = JAXBContext.newInstance(Customers.class);
-        //Create Unmarshal
-        Unmarshaller jaxbUnmarshall = jaxbContext.createUnmarshaller();
-        //Class which need to convert
-        Customers customers = (Customers) jaxbUnmarshall.unmarshal(xmlFile);
+        JAXBContext jaxbContext;
+        try {
+            //Create JAXB context
+            jaxbContext = JAXBContext.newInstance(Customers.class);
+            //Create Unmarshal
+            Unmarshaller jaxbUnmarshall = jaxbContext.createUnmarshaller();
+            //Class which need to convert
+            Customers customers = (Customers) jaxbUnmarshall.unmarshal(xmlFile);
 
-        for(Customer customer : customers.getCustomers())
-        {
-            System.out.println("[" + "Name: " + customer.getFirstName() + ", " + "Last name: " + customer.getLastName() + "]");
+            for (Customer customer : customers.getCustomers()) {
+                System.out.println("[" + "Name: " + customer.getFirstName() + ", " + "Last name: " + customer.getLastName() + "]");
+                customerRepository.save(customer);
+            }
+        } catch (JAXBException e) {
+            e.printStackTrace();
         }
     }
-    catch (JAXBException e)
-    {
-        e.printStackTrace();
-    }
-}
 }
